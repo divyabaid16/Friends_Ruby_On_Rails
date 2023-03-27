4c8ae3c4e5f6a2c29c6e8fb4d832aded33e789fd
divyabaid16 <divyabaid16@gmail.com>
2023-03-27 21:30:35 +0400

    Trying controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   end
 
   def about
+    # @about_me - instance variable (work everywhere) about_me - local variable
+    @about_me = "My name is Divya"
+    @answer = 2 + 2
   end
 end
